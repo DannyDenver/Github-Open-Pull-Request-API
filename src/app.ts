ef2f6cb8cd444cd73ee9b66b5556a1a898324b96
danieltaylor <danny.denver80204@gmail.com>
2022-03-24 20:52:42 -0600

    add axios get requests to github

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,43 @@
 import express, { Request, Response } from 'express';
+import OpenPullRequest from './models'
+import axios, { AxiosResponse } from 'axios';
+require('dotenv').config()
 
 export const app = express();
 app.use(express.json());
+
+const headers = process.env.TOKEN && {
+    Authorization: `token ${process.env.TOKEN}`
+}
+
+app.get("/api/v1/repos/:owner/:repo/pulls", (req: Request, res: Response) => {
+    axios.get(`https://api.github.com/repos/${req.params.owner}/${req.params.repo}/pulls?state=opened`, {  headers })
+        .then((pullRequests: AxiosResponse) => pullRequests.data)
+        .then((pullRequests: any[]) => {
+            const requests = pullRequests
+                                .map(pull => axios
+                                                .get(`https://api.github.com/repos/${req.params.owner}/${req.params.repo}/pulls/${pull.number}`,
+                                                    { headers }));
+            axios
+                .all(requests)
+                .then(axios.spread((...data) => {
+                    res.status(200).json(data.map((d:any, i:number) => 
+                        new OpenPullRequest(
+                            +pullRequests[i].id,
+                            +pullRequests[i].number,
+                            pullRequests[i].title,
+                            pullRequests[i].user.login,
+                            +d.data.commits
+                        )
+                    ));
+                }))
+                .catch((error: Error) => {
+                    res.status(500).send('Error: ' + error.message);
+                });;
+        })
+        .catch((error: Error) => {
+            res.status(500).send('Error: ' + error.message);
+        });
+});
+
+module.exports.app = app;
